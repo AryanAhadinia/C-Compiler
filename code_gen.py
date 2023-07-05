f2cfcb1666a6de839098fa801b941015c9559c30
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2023-07-05 22:45:01 +0330

    add

--- code_gen.py
+++ code_gen.py
@@ -2,20 +2,9 @@ class CodeGenerator:
     def __init__(self, parser, lexer) -> None:
         self.parser = parser
         self.lexer = lexer
+        self.semantic_stack = list()
+        self.jump_address = dict()
+        self.codes_generated = list()
 
     def code_gen(self, action) -> None:
-        if action == "#add":
-            self.action_add()
-        elif action == "#sub":
-            self.action_sub()
-        elif action == "#mul":
-            self.action_mul()
-
-    def action_add(self) -> None:
-        pass
-
-    def action_sub(self) -> None:
-        pass
-
-    def action_mul(self) -> None:
         pass
